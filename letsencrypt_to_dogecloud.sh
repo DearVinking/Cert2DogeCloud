789e5335c386708052e25e8cfaf40b0976de32ed
Vinking <54354339+DearVinking@users.noreply.github.com>
2024-12-26 15:49:18 +0800

    Add optional feature to delete old certificates
    - Improved performance by introducing parallel processing for certificate binding to domains, significantly reducing execution time.
    - Introduced a flag (DELETE_OLD_CERT) to control whether old certificates should be deleted after binding a new one.

--- letsencrypt_to_dogecloud.sh
+++ letsencrypt_to_dogecloud.sh
@@ -8,6 +8,9 @@ SECRET_KEY="xxxxxx"
 FULLCHAIN_PATH="/www/server/panel/vhost/ssl/xxxxxxx/fullchain.pem"
 PRIVKEY_PATH="/www/server/panel/vhost/ssl/xxxxxxx/privkey.pem"
 
+# 是否删除旧证书
+DELETE_OLD_CERT=false
+
 # 证书备注名
 CURRENT_DATE=$(date +"%y/%m/%d")
 NOTE="Certificate $CURRENT_DATE"
@@ -15,15 +18,63 @@ NOTE="Certificate $CURRENT_DATE"
 # 需要绑定的域名列表
 DOMAINS=("xxxxx.com" "cdn.xxxxx.com" "www.xxxxx.com")
 
+ACCESS_TOKEN_CACHE=""
+
 # 生成AccessToken
 function generateAccessToken() {
     local apiPath="$1"
     local body="$2"
-    local signStr=$(echo -e "${apiPath}\n${body}")
-    local sign=$(echo -n "$signStr" | openssl dgst -sha1 -hmac "$SECRET_KEY" | awk '{print $NF}')
-    local accessToken="$ACCESS_KEY:$sign"
 
-    echo "$accessToken"
+    if [ -z "$ACCESS_TOKEN_CACHE" ]; then
+        local signStr=$(echo -e "${apiPath}\n${body}")
+        local sign=$(echo -n "$signStr" | openssl dgst -sha1 -hmac "$SECRET_KEY" | awk '{print $NF}')
+        ACCESS_TOKEN_CACHE="$ACCESS_KEY:$sign"
+    fi
+
+    echo "$ACCESS_TOKEN_CACHE"
+}
+
+function parallelRequest() {
+    local url="$1"
+    local body="$2"
+    local apiPath="$3"
+    local accessToken=$(generateAccessToken "$apiPath" "$body")
+
+    curl -s -X POST "$url" \
+        -H "Authorization: TOKEN $accessToken" \
+        -H "Content-Type: application/x-www-form-urlencoded" \
+        --data "$body"
+}
+
+# 获取域名当前绑定的证书ID
+function getCurrentCertId() {
+    local domain="$1"
+    local body="domain=$domain"
+    local apiPath="/cdn/domain/info.json"
+    local response=$(parallelRequest "https://api.dogecloud.com/cdn/domain/info.json" "$body" "$apiPath")
+
+    local code=$(echo "$response" | jq -r '.code')
+    if [ "$code" -eq 200 ]; then
+        echo "$(echo "$response" | jq -r '.data.cert_id')"
+    else
+        echo ""
+    fi
+}
+
+# 删除证书
+function deleteCert() {
+    local certId="$1"
+    local body="id=$certId"
+    local apiPath="/cdn/cert/delete.json"
+    local response=$(parallelRequest "https://api.dogecloud.com/cdn/cert/delete.json" "$body" "$apiPath")
+
+    local code=$(echo "$response" | jq -r '.code')
+    if [ "$code" -eq 200 ]; then
+        echo "证书ID $certId 删除成功。"
+    else
+        local errMsg=$(echo "$response" | jq -r '.msg')
+        echo "证书ID $certId 删除失败，错误代码：$code，错误信息：$errMsg"
+    fi
 }
 
 # 上传证书到多吉云
@@ -31,19 +82,17 @@ function uploadCert() {
     local note="$1"
     local certFile="$2"
     local privateKeyFile="$3"
+
     local certContent=$(<"$certFile")
     local privateKeyContent=$(<"$privateKeyFile")
     local encodedCert=$(echo "$certContent" | jq -sRr @uri)
     local encodedPrivateKey=$(echo "$privateKeyContent" | jq -sRr @uri)
+
     local body="note=$note&cert=$encodedCert&private=$encodedPrivateKey"
-    local accessToken=$(generateAccessToken "/cdn/cert/upload.json" "$body")
-    local response=$(curl -s -X POST "https://api.dogecloud.com/cdn/cert/upload.json"  \
-         -H "Authorization: TOKEN $accessToken" \
-         -H "Content-Type: application/x-www-form-urlencoded" \
-         --data "$body")
+    local apiPath="/cdn/cert/upload.json"
+    local response=$(parallelRequest "https://api.dogecloud.com/cdn/cert/upload.json" "$body" "$apiPath")
 
     local code=$(echo "$response" | jq -r '.code')
-
     if [ "$code" -eq 200 ]; then
         echo "证书上传成功！"
         local certId=$(echo "$response" | jq -r '.data.id')
@@ -58,18 +107,24 @@ function uploadCert() {
 # 绑定证书到域名
 function bindCert() {
     local certId="$1"
-    local responses=()
+    declare -A oldCertIdsMap=()
 
+    # 获取每个域名当前绑定的证书ID
+    for domain in "${DOMAINS[@]}"; do
+        local currentCertId=$(getCurrentCertId "$domain")
+        if [ -n "$currentCertId" ]; then
+            oldCertIdsMap["$currentCertId"]=1
+        fi
+    done
+
+    # 绑定新证书
     for domain in "${DOMAINS[@]}"; do
         (
             local body="id=$certId&domain=$domain"
-            local accessToken=$(generateAccessToken "/cdn/cert/bind.json" "$body")
-            local response=$(curl -s -X POST "https://api.dogecloud.com/cdn/cert/bind.json"  \
-                 -H "Authorization: TOKEN $accessToken" \
-                 -H "Content-Type: application/x-www-form-urlencoded" \
-                 --data "$body")
-            local code=$(echo "$response" | jq -r '.code')
+            local apiPath="/cdn/cert/bind.json"
+            local response=$(parallelRequest "https://api.dogecloud.com/cdn/cert/bind.json" "$body" "$apiPath")
 
+            local code=$(echo "$response" | jq -r '.code')
             if [ "$code" -eq 200 ]; then
                 echo "证书已成功绑定到 $domain"
             else
@@ -78,8 +133,15 @@ function bindCert() {
             fi
         ) &
     done
-
     wait
+
+    if [ "$DELETE_OLD_CERT" = true ]; then
+        for oldCertId in "${!oldCertIdsMap[@]}"; do
+            if [ "$oldCertId" != "$certId" ]; then
+                deleteCert "$oldCertId"
+            fi
+        done
+    fi
 }
 
 uploadCert "$NOTE" "$FULLCHAIN_PATH" "$PRIVKEY_PATH"
